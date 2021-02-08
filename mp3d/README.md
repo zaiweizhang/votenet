215c014b30e9f456d91be126f9d98d0f7d034605
zaiweizhang <zaiweizhang@utexas.edu>
2021-02-08 14:53:11 -0600

    new script for matterport

--- mp3d/README.md
+++ mp3d/README.md
@@ -2,4 +2,4 @@
 
 1. Download Matterport3D data [HERE](https://niessner.github.io/Matterport/). Move/link the `scans` folder such that under `scans` there should be folders with names such as `scene0001_01`.
 
-2. Matterport3D follows the same format with ScanNet V2. Please use the same script to extract point clouds and annotations (semantic seg, instance seg etc.) by running `python batch_load_scannet_data.py`. Please change the path accordingly.
+2. Matterport3D follows similar data format with ScanNet V2. Please use the provided script "process_matterport.py" to extract point clouds and annotations (semantic seg, instance seg etc.). Please change the path accordingly.
---
+++ mp3d/process_matterport.py
@@ -0,0 +1,114 @@
+import numpy as np 
+import os 
+from plyfile import PlyData, PlyElement
+import json
+import sys 
+#sys.path.append('/home/bo/projects/cvpr2020/detection/new/votenet/scannet') ### Path to matterport data
+
+def read_mesh_vertices_rgb(filename):
+    """ read XYZ RGB for each vertex.
+    Note: RGB values are in 0-255
+    """
+    assert os.path.isfile(filename)
+    with open(filename, 'rb') as f:
+        plydata = PlyData.read(f)
+        num_verts = plydata['vertex'].count
+        vertices = np.zeros(shape=[num_verts, 6], dtype=np.float32)
+        vertices[:,0] = plydata['vertex'].data['x']
+        vertices[:,1] = plydata['vertex'].data['y']
+        vertices[:,2] = plydata['vertex'].data['z']
+        vertices[:,3] = plydata['vertex'].data['red']
+        vertices[:,4] = plydata['vertex'].data['green']
+        vertices[:,5] = plydata['vertex'].data['blue']
+    return vertices
+
+def read_aggregation(filename):
+    assert os.path.isfile(filename)
+    object_id_to_segs = {}
+    label_to_segs = {}
+    with open(filename) as f:
+        data = json.load(f)
+        num_objects = len(data['segGroups'])
+        for i in range(num_objects):
+            object_id = data['segGroups'][i]['objectId'] + 1 # instance ids should be 1-indexed
+            label = data['segGroups'][i]['label']
+            segs = data['segGroups'][i]['segments']
+            object_id_to_segs[object_id] = segs
+            if label in label_to_segs:
+                label_to_segs[label].extend(segs)
+            else:
+                label_to_segs[label] = segs
+    return object_id_to_segs, label_to_segs
+
+def read_segmentation(filename):
+    assert os.path.isfile(filename)
+    seg_to_verts = {}
+    with open(filename) as f:
+        data = json.load(f)
+        num_verts = len(data['segIndices'])
+        for i in range(num_verts):
+            seg_id = data['segIndices'][i]
+            if seg_id in seg_to_verts:
+                seg_to_verts[seg_id].append(i)
+            else:
+                seg_to_verts[seg_id] = [i]
+    return seg_to_verts, num_verts
+
+
+data_path = 'scans' # Path to matterport scans
+LABEL_MAP_FILE = '' #'/home/bo/data/Matterport/metadata/category_mapping.tsv' # Path to category_mapping.tsv file
+label_map = scannet_utils.read_label_mapping(LABEL_MAP_FILE,
+        label_from='raw_category', label_to='nyu40id')  
+
+
+for house_name in os.listdir(data_path):
+    for tmp_name in os.listdir(os.path.join(data_path, house_name, house_name, 'region_segmentations')):
+        if not tmp_name.endswith('.ply'):
+            continue
+        region_name = tmp_name[:-4]
+        print(house_name, region_name)
+        mesh_vertices = read_mesh_vertices_rgb(os.path.join(data_path, house_name, house_name, 'region_segmentations', region_name+'.ply'))
+        fseg_filename = os.path.join(data_path, house_name, house_name, 'region_segmentations', region_name+'fsegs.json')
+        semseg_filename = os.path.join(data_path, house_name, house_name, 'region_segmentations', region_name+'semseg.json')
+        object_id_to_segs, label_to_segs = read_aggregation(semseg_filename)
+        seg_to_verts, num_verts = read_segmentation(fseg_filename)
+        
+        label_ids = np.zeros(shape=(num_verts), dtype=np.uint32) # 0: unannotated      
+        object_id_to_label_id = {}
+        for label, segs in label_to_segs.items():
+            label_id = label_map[label]
+            for seg in segs:
+                verts = seg_to_verts[seg]
+                label_ids[verts] = label_id
+        instance_ids = np.zeros(shape=(num_verts), dtype=np.uint32) # 0: unannotated
+        num_instances = len(np.unique(list(object_id_to_segs.keys())))
+        for object_id, segs in object_id_to_segs.items():
+            for seg in segs:
+                verts = seg_to_verts[seg]
+                instance_ids[verts] = object_id
+                if object_id not in object_id_to_label_id:
+                    object_id_to_label_id[object_id] = label_ids[verts][0]
+            instance_bboxes = np.zeros((num_instances,7))
+        
+        for obj_id in object_id_to_segs:
+            label_id = object_id_to_label_id[obj_id]
+            obj_pc = mesh_vertices[instance_ids==obj_id, 0:3]
+            if len(obj_pc) == 0: continue
+            xmin = np.min(obj_pc[:,0])
+            ymin = np.min(obj_pc[:,1])
+            zmin = np.min(obj_pc[:,2])
+            xmax = np.max(obj_pc[:,0])
+            ymax = np.max(obj_pc[:,1])
+            zmax = np.max(obj_pc[:,2])
+            bbox = np.array([(xmin+xmax)/2, (ymin+ymax)/2, (zmin+zmax)/2,
+                xmax-xmin, ymax-ymin, zmax-zmin, label_id])
+            # NOTE: this assumes obj_id is in 1,2,3,.,,,.NUM_INSTANCES
+            instance_bboxes[obj_id-1,:] = bbox 
+            
+        output_file = 'ours/{}_{}'.format(house_name, region_name)
+        np.save(output_file+'_vert.npy', mesh_vertices)
+        np.save(output_file+'_sem_label.npy', label_ids)
+        np.save(output_file+'_ins_label.npy', instance_ids)
+        np.save(output_file+'_bbox.npy', instance_bboxes)
+                
+    # os.system('unzip %s'%(os.path.join(data_path, house_name, 'region_segmentations.zip')))
